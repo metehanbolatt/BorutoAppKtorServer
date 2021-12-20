4a2b401a572b00bdedea50df9e85db3faf09ebdc
metehanie <metehan.bolat.ie@gmail.com>
2021-12-21 02:12:01 +0300

    Status Pages

--- src/main/kotlin/com/metehanbolat/Application.kt
+++ src/main/kotlin/com/metehanbolat/Application.kt
@@ -13,4 +13,5 @@ fun Application.module() {
     configureSerialization()
     configureMonitoring()
     configureDefaultHeader()
+    configureStatusPages()
 }
--- src/main/kotlin/com/metehanbolat/plugins/Routing.kt
+++ src/main/kotlin/com/metehanbolat/plugins/Routing.kt
@@ -4,11 +4,8 @@ import com.metehanbolat.routes.getAllHeroes
 import com.metehanbolat.routes.root
 import com.metehanbolat.routes.searchHeroes
 import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
 import io.ktor.http.content.*
-import io.ktor.response.*
-import io.ktor.request.*
 
 fun Application.configureRouting() {
 
