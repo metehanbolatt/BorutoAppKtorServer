af788b070791f52dfb40552b0fe38f8c164c7800
metehanie <metehan.bolat.ie@gmail.com>
2021-12-21 02:14:26 +0300

    Status Pages - delete unnecessary import

--- src/main/kotlin/com/metehanbolat/plugins/Monitoring.kt
+++ src/main/kotlin/com/metehanbolat/plugins/Monitoring.kt
@@ -1,10 +1,7 @@
 package com.metehanbolat.plugins
 
 import io.ktor.features.*
-import org.slf4j.event.*
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
 
 fun Application.configureMonitoring() {
     install(CallLogging)
--- src/main/kotlin/com/metehanbolat/plugins/Serialization.kt
+++ src/main/kotlin/com/metehanbolat/plugins/Serialization.kt
@@ -3,9 +3,6 @@ package com.metehanbolat.plugins
 import io.ktor.serialization.*
 import io.ktor.features.*
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
