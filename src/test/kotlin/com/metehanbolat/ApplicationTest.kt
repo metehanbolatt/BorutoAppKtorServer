9936c9b0cd51ca5f1bc1dbbf4a6b68eb7d015524
metehanie <metehan.bolat.ie@gmail.com>
2021-12-22 02:12:15 +0300

    `access all heroes endpoint, assert correct information` tested.

--- src/test/kotlin/com/metehanbolat/ApplicationTest.kt
+++ src/test/kotlin/com/metehanbolat/ApplicationTest.kt
@@ -1,13 +1,21 @@
 package com.metehanbolat
 
+import com.metehanbolat.models.ApiResponse
+import com.metehanbolat.repository.HeroRepository
 import io.ktor.http.*
 import io.ktor.application.*
 import kotlin.test.*
 import io.ktor.server.testing.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.koin.java.KoinJavaComponent.inject
 
 class ApplicationTest {
+
+    private val heroRepository: HeroRepository by inject(HeroRepository::class.java)
+
     @Test
-    fun testRoot() {
+    fun `access root endpoint, assert correct information`() {
         withTestApplication(moduleFunction = Application::module) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(
@@ -17,6 +25,31 @@ class ApplicationTest {
                 assertEquals(
                     expected = "Welcome to Boruto API!",
                     actual = response.content
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `access all heroes endpoint, assert correct information`(){
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status()
+                )
+                val expected = ApiResponse(
+                    success = true,
+                    message = "ok",
+                    prevPage = null,
+                    nextPage = 2,
+                    heroes = heroRepository.page1,
+                )
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+
+                assertEquals(
+                    expected = expected,
+                    actual = actual
                 )
             }
         }
